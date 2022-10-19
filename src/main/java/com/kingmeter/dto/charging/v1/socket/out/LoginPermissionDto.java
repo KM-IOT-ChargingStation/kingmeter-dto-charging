bd98008a483b33281e548bc1bce21015e2147dbb
crazyandy <auden.sh@163.com>
2022-10-19 20:42:06 +0800

    add @NoArgsConstructor for LoginPermissionDto

--- src/main/java/com/kingmeter/dto/charging/v1/socket/out/LoginPermissionDto.java
+++ src/main/java/com/kingmeter/dto/charging/v1/socket/out/LoginPermissionDto.java
@@ -2,9 +2,10 @@ package com.kingmeter.dto.charging.v1.socket.out;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class LoginPermissionDto {
     private LoginResponseDto responseDto;
