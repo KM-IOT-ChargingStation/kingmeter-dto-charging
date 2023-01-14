880f3324d0b43c685f8e860d5559fb6a15f1589f
crazyandy <auden.sh@163.com>
2023-01-14 16:33:56 +0800

    add confirm dto for ScanUnlock C105

---
+++ src/main/java/com/kingmeter/dto/charging/v1/socket/out/ScanUnLockConfirmResponseDto.java
@@ -0,0 +1,14 @@
+package com.kingmeter.dto.charging.v1.socket.out;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: crazyandy
+ */
+@AllArgsConstructor
+@Data
+public class ScanUnLockConfirmResponseDto {
+    private long kid;//桩体id
+}
